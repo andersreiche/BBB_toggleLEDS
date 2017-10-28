20c76673243fdf698ed3b52958dbb4f1b2ff5405
root <root@beaglebone.localdomain>
2017-10-28 21:21:10 +0000

    added toggle functionality to individual LEDs

--- toggleLEDS.sh
+++ toggleLEDS.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-#
 
 LED0=/sys/class/leds/beaglebone:green:usr0/brightness
 LED1=/sys/class/leds/beaglebone:green:usr1/brightness
@@ -13,13 +11,13 @@ LED3_STATE=$(cat "$LED3")
 
 if [ $# -eq 0 ]; then
 	echo "Usage:		./toggleLEDS <on/off/toggle>"
-	echo "Alternatively: 	./toggleLEDS <0-3> <on/off>"
+	echo "Alternatively: 	./toggleLEDS <0-3> <on/off/toggle>"
 	exit 2
 fi
 
 #Switch all LEDS ON
 if [ "$1" == "on" ]; then
-	echo "All LEDS are ON!"
+	echo "All LEDS ON!"
 	echo 1 >> "$LED0"
 	echo 1 >> "$LED1"
 	echo 1 >> "$LED2"
@@ -28,7 +26,7 @@ fi
 
 #Switch all LEDS OFF
 if [ "$1" == "off" ]; then
-	echo "All LEDS are OFF!"
+	echo "All LEDS OFF!"
 	echo 0 >> "$LED0"
 	echo 0 >> "$LED1"
 	echo 0 >> "$LED2"
@@ -37,63 +35,92 @@ fi
 
 #Switch LED0 ON
 if [ "$1" == "0" ] && [ "$2" == "on" ]; then
-	echo "Toggled LED0 ON!"
+	echo "LED0 ON!"
 	echo 1 >> "$LED0"
 fi
 
 #Switch LED0 OFF
 if [ "$1" == "0" ] && [ "$2" == "off" ]; then
-	echo "Toggled LED0 OFF!"
+	echo "LED0 OFF!"
 	echo 0 >> "$LED0"
 fi
 
 #Switch LED1 ON
 if [ "$1" == "1" ] && [ "$2" == "on" ]; then
-	echo "Toggled LED1 ON!"
+	echo "LED1 ON!"
 	echo 1 >> "$LED1"
 fi
 
 #Switch LED1 OFF
 if [ "$1" == "1" ] && [ "$2" == "off" ]; then
-	echo "Toggled LED1 OFF!"
+	echo "LED1 OFF!"
 	echo 0 >> "$LED1"
 fi
 
 #Switch LED2 ON
 if [ "$1" == "2" ] && [ "$2" == "on" ]; then
-	echo "Toggled LED2 ON!"
+	echo "LED2 ON!"
 	echo 1 >> "$LED2"
 fi
 
 #Switch LED2 OFF
 if [ "$1" == "2" ] && [ "$2" == "off" ]; then
-	echo "Toggled LED2 OFF!"
+	echo "LED2 OFF!"
 	echo 0 >> "$LED2"
 fi
 
 #Switch LED3 ON
 if [ "$1" == "3" ] && [ "$2" == "on" ]; then
-	echo "Toggled LED3 ON!"
+	echo "LED3 ON!"
 	echo 1 >> "$LED3"
 fi
 
 #Switch LED3 OFF
 if [ "$1" == "3" ] && [ "$2" == "off" ]; then
-	echo "Toggled LED3 Off!"
+	echo "LED3 Off!"
 	echo 0 >> "$LED3"
 fi
 
-#Toggle all LEDS
+#Toggle all LEDs
 if [ "$1" == "toggle" ]; then
-	if [[ LED0_STATE -eq 0 ]]; then echo 1 > "$LED0"
-	else echo 0 > "$LED0"
+	echo "all LEDs toggled"
+	if [[ LED0_STATE -eq 0 ]]; then echo 1 > "$LED0" 
+	else echo 0 > "$LED0" 
 	fi
+	if [[ LED1_STATE -eq 0 ]]; then echo 1 > "$LED1" 
+	else echo 0 > "$LED1" 
+	fi
+	if [[ LED2_STATE -eq 0 ]]; then echo 1 > "$LED2" 
+	else echo 0 > "$LED2" 
+	fi
+	if [[ LED3_STATE -eq 0 ]]; then echo 1 > "$LED3" 
+	else echo 0 > "$LED3" 
+	fi
+fi
+
+#Toggle LED0
+if [ "$1" == "0" ] && [ "$2" == "toggle" ]; then
+	if [[ LED0_STATE -eq 0 ]]; then echo 1 > "$LED0" 
+	else echo 0 > "$LED0" 
+	fi
+fi
+
+#Toggle LED1
+if [ "$1" == "1" ] && [ "$2" == "toggle" ]; then
 	if [[ LED1_STATE -eq 0 ]]; then echo 1 > "$LED1"
 	else echo 0 > "$LED1"
 	fi
+fi
+
+#Toggle LED2
+if [ "$1" == "2" ] && [ "$2" == "toggle" ]; then
 	if [[ LED2_STATE -eq 0 ]]; then echo 1 > "$LED2"
 	else echo 0 > "$LED2"
 	fi
+fi
+
+#Toggle LED3
+if [ "$1" == "3" ] && [ "$2" == "toggle" ]; then
 	if [[ LED3_STATE -eq 0 ]]; then echo 1 > "$LED3"
 	else echo 0 > "$LED3"
 	fi
